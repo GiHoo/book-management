a3106f2e478950f66b59f98b2a3bf345148dbe8d
kimnamgyu <lethargic.hood@gmail.com>
2023-11-04 22:07:20 +0900

    Refactor : 필드명 수정

--- src/main/java/com/programmers/bookmanagement/model/Book.java
+++ src/main/java/com/programmers/bookmanagement/model/Book.java
@@ -8,15 +8,15 @@ import java.util.UUID;
 @Getter
 public class Book {
     private final UUID bookId;
-    private final String productName;
+    private final String bookName;
     private final Category category;
     private final long price;
     private final String description;
     private final LocalDateTime createdAt;
 
-    public Book(UUID bookId, String productName, Category category, long price, String description) {
+    public Book(UUID bookId, String bookName, Category category, long price, String description) {
         this.bookId = bookId;
-        this.productName = productName;
+        this.bookName = bookName;
         this.category = category;
         this.price = price;
         this.description = description;
