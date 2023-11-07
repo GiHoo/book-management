3ef365fabdf6719e4f035451f5fc9eb3c5bf946f
kimnamgyu <lethargic.hood@gmail.com>
2023-11-07 19:23:19 +0900

    feat : Order 생성자(생성, 수정 시간 기입을 위한)추가

--- src/main/java/com/programmers/bookmanagement/model/Order.java
+++ src/main/java/com/programmers/bookmanagement/model/Order.java
@@ -30,6 +30,18 @@ public class Order {
         this.updatedAt = LocalDateTime.now();
     }
 
+    public Order(UUID orderId, String name, Email email, String address, String postcode, List<OrderItem> orderItems, OrderStatus orderStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.orderId = orderId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.orderItems = orderItems;
+        this.orderStatus = orderStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
     }
